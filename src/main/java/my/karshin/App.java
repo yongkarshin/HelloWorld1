6dfb1656c42d4fda8cb8e77e2a90ce794b1d9a89
yongkarshin <57005366+yongkarshin@users.noreply.github.com>
2021-04-07 12:09:14 +0800

    Initial commit

---
+++ src/main/java/my/karshin/App.java
@@ -0,0 +1,19 @@
+package my.karshin;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App(){
+        setTitle("Hello World");
+        setSize(600, 400);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
